35d58e4a2365a58d0c4563e9d7161d13b4917de6
rajghugare19 <rajghugare.vnit@gmail.com>
2025-11-22 22:21:51 +0000

    adding latest script

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 
 PYTHON_SCRIPT="ppo.py"
-GPU_IDS=(6 7)
-SEEDS=(42 43)
-EPISODE_LENGTH=1000
+NUM_CUBES=(3 3 4 4)
+GPU_IDS=(0 1 2 3)
+SEEDS=(42 43 42 43)
+EPISODE_LENGTH=500
 
 # Get the number of instances from the length of the GPU array
 NUM_INSTANCES=${#GPU_IDS[@]}
@@ -17,18 +18,18 @@ for i in $(seq 0 $((NUM_INSTANCES - 1))); do
     # Get the specific GPU and ENV ID for this instance
     GPU_ID=${GPU_IDS[$i]}
     SEED=${SEEDS[$i]}
-
+    NUM_CUBE=${NUM_CUBES[$i]}
+    
     (
         cd ..
-        
-        echo "Starting instance with SEED $SEED on CUDA device: $GPU_ID (from $(pwd))..."
+
+        echo "Starting instance with SEED $SEED on task creative-$NUM_CUBE on CUDA device: $GPU_ID (from $(pwd))..."
 
         # Run the command from the parent directory
         CUDA_VISIBLE_DEVICES=$GPU_ID python $PYTHON_SCRIPT \
-            --env_id="creative-4-task1" \
-            --wandb_name_tag="no-permutation_invariant_reward" \
+            --env_id="creative-$NUM_CUBE-task1" \
+            --wandb_name_tag="direct-force-control" \
             --seed=$SEED \
-            --no-permutation_invariant_reward \
             --env_episode_length=$EPISODE_LENGTH \
             --num_timesteps=3000000000 \
             --track
